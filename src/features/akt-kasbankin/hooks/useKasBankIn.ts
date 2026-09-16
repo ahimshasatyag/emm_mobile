337545d394ac062240302bff31e1b37b1793ee72
himsha <himsha@ekamaju.co.id>
2026-09-16 10:26:41 +0700

    feat: add useKasBankIn custom hook and Matauang currency model

--- src/features/akt-kasbankin/hooks/useKasBankIn.ts
+++ src/features/akt-kasbankin/hooks/useKasBankIn.ts
@@ -1,18 +1,21 @@
 import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState, AppDispatch } from '../../../stores';
-import { 
-    fetchKasBankIns, 
-    fetchMasterData, 
-    fetchKasBankInById, 
-    saveKasBankIn, 
-    clearCurrentKasBankIn 
+import {
+    fetchKasBankIns,
+    fetchMasterData,
+    fetchKasBankInById,
+    saveKasBankIn,
+    clearCurrentKasBankIn
 } from '../stores/kasbankinSlice';
 import { KasBankInHeader, KasBankInDetail } from '../types/kasbankin.types';
+import { notificationService } from '../../../services/notification/notificationService';
+import { formatRp } from '../../../utils/helpers/money';
 
 export const useKasBankIn = () => {
     const dispatch = useDispatch<AppDispatch>();
-    
+    const authUser = useSelector((state: RootState) => state.auth.user);
+
     const {
         kasBankIns,
         banks,
@@ -38,8 +41,23 @@ export const useKasBankIn = () => {
     }, [dispatch]);
 
     const submitKasBankIn = useCallback(async (data: { header: Partial<KasBankInHeader>, details: Partial<KasBankInDetail>[] }) => {
-        await dispatch(saveKasBankIn(data)).unwrap();
-    }, [dispatch]);
+        const result = await dispatch(saveKasBankIn(data)).unwrap();
+
+        const isUpdate = !!data.header.id_kb_masuk;
+        const tipeKas = data.header.type_kb === 'k' ? 'Kas' : 'Bank';
+        const msg = `Penerimaan ${tipeKas} sejumlah ${formatRp(data.header.v_amount || 0)} berhasil ${isUpdate ? 'diperbarui' : 'ditambahkan'} oleh ${authUser?.nm_users}`;
+
+        await notificationService.store({
+            user_id: authUser?.id_user ?? 1,
+            id_users_level: authUser?.id_users_level ?? 1,
+            kode_trans: data.header.code_kb_masuk ?? '',
+            judul: isUpdate ? 'Kas/Bank Masuk Diperbarui' : 'Kas/Bank Masuk Baru',
+            pesan: msg,
+            action: isUpdate ? 'Update' : 'Create'
+        }).catch(() => { });
+
+        return result;
+    }, [dispatch, authUser]);
 
     const resetCurrent = useCallback(() => {
         dispatch(clearCurrentKasBankIn());
@@ -49,10 +67,10 @@ export const useKasBankIn = () => {
         if (!headerData.id_bank) return "Bank/Kas harus dipilih!";
         if (headerData.f_dp && !headerData.id_so) return "No. SO harus dipilih jika tipe DP!";
         if (detailData.length === 0) return "Minimal 1 detail COA harus diisi!";
-        
+
         const totalDetail = detailData.reduce((sum, item) => sum + (item.v_amount || 0), 0);
         if (totalDetail !== (headerData.v_amount || 0)) return "Total nilai detail harus sama dengan Total (Amount)!";
-        
+
         return null;
     };
 
